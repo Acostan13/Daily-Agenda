fb7a70bd897b4299be180e42f06e8cdca942b7e8
Acostan13 <acostan@fau.edu>
2020-06-23 09:56:04 -0400

    styling react components

--- start-here/src/index.js
+++ start-here/src/index.js
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-//JSX
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import "./index.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol lg="6">Left column</MDBCol>
+            <MDBCol lg="6">Right column</MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </React.Fragment>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
